f818a27b22741eb6a5d3b0a743a41b554e9d5f21
Adauby <adaubyassi@gmail.com>
2020-04-09 12:24:04 +0000

    Add tps to Esme account

--- src/main/java/com/carrier/util/Values.java
+++ src/main/java/com/carrier/util/Values.java
@@ -6,6 +6,7 @@ public class Values {
 	public static final long DEFAULT_CARRIER_WINDOW_MONITOR_TIMEOUT=15000;
 	public static final int DEFAULT_ENQUIRE_LINK_INTERVAL = 15000;
 	public static final long ONE_SECOND_IN_MILLIS=1000;
+	public static final int DEFAULT_TPS = 100;
 
 	private Values() {
 
