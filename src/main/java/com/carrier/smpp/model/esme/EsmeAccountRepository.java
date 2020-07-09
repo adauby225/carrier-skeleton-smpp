ba275c7c8eb4ba9da04b164fcdf0af4a68951c2b
Adauby <adaubyassi@gmail.com>
2020-07-09 10:51:51 +0000

    find ESME account using SMPP session config

--- src/main/java/com/carrier/smpp/model/esme/EsmeAccountRepository.java
+++ src/main/java/com/carrier/smpp/model/esme/EsmeAccountRepository.java
@@ -1,7 +1,11 @@
 package com.carrier.smpp.model.esme;
 
+import com.cloudhopper.smpp.SmppSessionConfiguration;
+
 public interface EsmeAccountRepository {
 
+	@Deprecated
 	EsmeSmppAccount findBySystemId(String systemId);
+	EsmeSmppAccount find(SmppSessionConfiguration sessionConfig);
 
 }
