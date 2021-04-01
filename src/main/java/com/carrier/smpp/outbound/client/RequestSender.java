bcd5ef1c80cdc20676165904699148baca829639
Adauby <adaubyassi@gmail.com>
2021-04-01 11:19:36 +0000

    Make RequestSender cloneable

--- src/main/java/com/carrier/smpp/outbound/client/RequestSender.java
+++ src/main/java/com/carrier/smpp/outbound/client/RequestSender.java
@@ -2,7 +2,7 @@ package com.carrier.smpp.outbound.client;
 
 import com.cloudhopper.smpp.SmppSession;
 
-public interface RequestSender {
+public interface RequestSender extends Cloneable{
 
 	void send(SmppSession session,PduQueue pduQueue, int tps)throws InterruptedException;
 	void send(SmppSession session,int requestInterval) throws InterruptedException;
