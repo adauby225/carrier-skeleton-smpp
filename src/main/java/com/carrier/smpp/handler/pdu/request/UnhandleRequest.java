be318c8823a025beabd9558aa7fc9e57bfe3f9b7
Adauby <adaubyassi@gmail.com>
2021-05-21 19:22:12 +0000

    Implement unhandle request

--- src/main/java/com/carrier/smpp/handler/pdu/request/UnhandleRequest.java
+++ src/main/java/com/carrier/smpp/handler/pdu/request/UnhandleRequest.java
@@ -7,12 +7,12 @@ import com.carrier.smpp.handler.pdu.response.DefaultPduRespHandler;
 import com.cloudhopper.smpp.pdu.PduRequest;
 import com.cloudhopper.smpp.pdu.PduResponse;
 
-public class UnhandleRequest implements RequestHandler<PduRequest,PduResponse> {
+public class UnhandleRequest implements RequestHandler<EsmePduRequest,PduResponse> {
 	private Logger logger = LogManager.getLogger(DefaultPduRespHandler.class.getName());
 	@Override
-	public PduResponse handleRequest(PduRequest pduRequest) {
-		
-		return null;
+	public PduResponse handleRequest(EsmePduRequest request) {
+		PduRequest pduRequest = request.getRequest();
+		return pduRequest.createResponse();
 	}
 
 }
